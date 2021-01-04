d673542d704fc01714607210be0909a633b47275
Jason Marckel <jason@marckel.net>
2021-01-04 00:25:25 -0500

    local storage for player name

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.LocalStorage;
 
 namespace Bingo.Client
 {
@@ -19,6 +20,8 @@ namespace Bingo.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddBlazoredLocalStorage();
+            
             await builder.Build().RunAsync();
         }
     }
